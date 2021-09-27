0701076358cb6c2d396d35841d95aef764361b66
Alberto R <39353600+ar7tech@users.noreply.github.com>
2021-09-26 23:03:18 -0300

    Update gerador_cpf_cnpj.py

--- gerador_cpf_cnpj.py
+++ gerador_cpf_cnpj.py
@@ -1,5 +1,6 @@
-# Versão 1.0.0
-# 13/12/2020
+# Versão 2.0.0
+# Criado em: 13/12/2020
+# Alterado em: 22/08/2021
 
 from random import randint
 
@@ -21,8 +22,8 @@ def CalcularDigitosCpf(n_cpf):
     multiplo = qnt + 1
     res_dv = 0
     for a in range(qnt):
-        n_cpf[a] = n_cpf[a] * multiplo
-        res_dv = res_dv + n_cpf[a]
+        n_cpf[a] = int(n_cpf[a]) * multiplo
+        res_dv = res_dv + int(n_cpf[a])
         multiplo = multiplo - 1
     dv = 11-(res_dv % 11)
     return ValidaDigito(dv)
@@ -54,7 +55,18 @@ def GerarCPF(formata):
     else:
         return 'Opção incorreta!'
 
-print(GerarCPF('S'))
+def ValidaCPF(n_cpf):
+    resto_cpf = n_cpf[0:9]
+    gerar_numero_cpf = list(resto_cpf)
+    cpf_dv_1 = CalcularDigitosCpf(gerar_numero_cpf.copy())
+    gerar_numero_cpf.append(cpf_dv_1)
+    cpf_dv_2 = CalcularDigitosCpf(gerar_numero_cpf.copy())
+    gerar_numero_cpf.append(cpf_dv_2)
+    resultado = ''.join(str(a) for a in gerar_numero_cpf)
+    if n_cpf == resultado:
+        return print('CPF Verdadeiro!')
+    else:
+        return print(f'CPF Falso, o CPF correto é: ', resultado)
 
 def CalcularDigitosCnpj(numeros):
     if len(numeros) == 12:
@@ -65,14 +77,14 @@ def CalcularDigitosCnpj(numeros):
     res_dv = 0
     n_cnpj = numeros[::-1]
     for c in range(qnt):
-        n_cnpj[c] = n_cnpj[c] * multiplo
-        res_dv = res_dv + n_cnpj[c]
+        n_cnpj[c] = int(n_cnpj[c]) * multiplo
+        res_dv = res_dv + int(n_cnpj[c])
         multiplo = multiplo + 1
     qnt = len(n_cnpj)
     multiplo = 2
     for d in range(8, qnt):
-        n_cnpj[d] = n_cnpj[d] * multiplo
-        res_dv = res_dv + n_cnpj[d]
+        n_cnpj[d] = int(n_cnpj[d]) * multiplo
+        res_dv = res_dv + int(n_cnpj[d])
         multiplo = multiplo + 1
     dv = 11-(res_dv % 11)
     return ValidaDigito(dv)
@@ -81,7 +93,7 @@ def FormatarCnpj(num_cnpj):
     teste = num_cnpj
     qnt = len(num_cnpj)
     for b in range(qnt):
-        if b == 1 or b == 5:
+        if b == 1 or b == 4:
             teste[b] = str(teste[b]) + '.'
         elif b == 7:
             teste[b] = str(teste[b]) + '/'
@@ -106,4 +118,23 @@ def GerarCNPJ(formata):
     else:
         return 'Opção incorreta!'
 
+def ValidaCNPJ(n_cnpj):
+    resto_cnpj = n_cnpj[0:12]
+    gerar_numero_cnpj = list(resto_cnpj)
+    cnpj_dv_1 = CalcularDigitosCnpj(gerar_numero_cnpj.copy())
+    gerar_numero_cnpj.append(cnpj_dv_1)
+    cnpj_dv_2 = CalcularDigitosCnpj(gerar_numero_cnpj.copy())
+    gerar_numero_cnpj.append(cnpj_dv_2)
+    resultado = ''.join(str(a) for a in gerar_numero_cnpj)
+    if n_cnpj == resultado:
+        return print('CNPJ Verdadeiro!')
+    else:
+        return print(f'CNPJ Falso, o CNPJ correto é: ', resultado)
+
+print(GerarCPF('S'))
+
 print(GerarCNPJ('S'))
+
+verificar_cpf = ValidaCPF(input('Digite o CPF para validação: '))
+
+verificar_cnpj = ValidaCNPJ(input('Digite o CNPJ para validação: '))
